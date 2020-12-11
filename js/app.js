40354f4af18ab37aa3dbe83c2250edb364728bd5
Ryan Talbert <ryantalbert@Ryans-Macintosh.local>
2020-12-11 14:20:01 -0600

    fixed the app

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import { closeSidebar, openEverything } from './closeFunctionality/index.js';
 
 (function app() {
-  const PAGE_URL = 'http://localhost:8080';
+  const PAGE_URL = 'https://geniecast-livestream-builder.herokuapp.com/';
   const slug = document.querySelector('.video-app').dataset.slug;
 
   let content = [];
@@ -27,9 +27,7 @@ import { closeSidebar, openEverything } from './closeFunctionality/index.js';
 
   async function grabSideBarContent() {
     try {
-      let foundData = await fetch(
-        process.env.PAGE_URL + '/v1/api/videos/' + slug
-      );
+      let foundData = await fetch(PAGE_URL + '/v1/api/videos/' + slug);
 
       let data = await foundData.json();
 
