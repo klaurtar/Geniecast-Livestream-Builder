1e2c6b3f6ee0ab8258dec3cbae4278197c2c5bf5
Ryan Talbert <ryantalbert@Ryans-Macintosh.local>
2020-12-16 10:15:06 -0600

    got rid of restriction

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -10,7 +10,7 @@ router.post('/admin/login', authController.login);
 
 //router.use(authController.isLoggedIn);
 
-router.use(authController.restrictTo(['admin', 'Super Admin']));
+//router.use(authController.restrictTo(['admin', 'Super Admin']));
 
 router.route('/').get(authController.isLoggedIn, videoControllers.getAllVideos);
 
